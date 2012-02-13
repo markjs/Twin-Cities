77e0ad15910afa9418b6538583ec99bb4671c396
Charlie Tizard <c2-tizard@cems.uwe.ac.uk>
2012-02-13 12:21:27 +0000

    twitter array

--- twitter.php
+++ twitter.php
@@ -2,17 +2,38 @@
 
 <?php
 
-include("commonlib.php");
-loadConfig("config.xml");
+include_once("commonlib.php");
+echo "<pre>";
+$config = loadConfig();
 
-$ed = $city1Lat . "," . $city1Lng;
+function twitterGetTweets($lat, $lng) { 
 
-$sd = $city2Lat . "," . $city2Lng;
+$config = loadConfig();
+$count = 0;
+$post =""; 
+$twitterFeed = simplexml_load_string(acquire_file('http://search.twitter.com/search.atom?geocode=' . $lat . "," . $lng . ',10mi&lang=en&include_entities=true&result_type=recent+exclude:retweets+exclude:replies'));
+	foreach($twitterFeed->entry as $tweet) {
+		
+		$post[] = (string) $tweet->author->name[0];
+		$post[] = (string) $tweet->author->uri[0];
+		$post[] = (string) $tweet->content[0];
 
-$twitter2 = simplexml_load_string(acquire_file('http://search.twitter.com/search.atom?geocode=' . $sd . ',10mi&lang=en&include_entities=true&result_type=recent'));
+		
 
-/* $twitter = simplexml_load_string(acquire_file('http://search.twitter.com/search.atom?geocode=32.71533,-117.157256,10mi&lang=en&include_entities=true&result_type=recent')); */
+	}
+return $post;
+}
 
-print_r ($twitter);
+print_r (twitterGetTweets($config['city1']['lat'], $config['city1']['lng']));
 
+
+
+
+
+
+
+
+
+ /* $twitter = simplexml_load_string(acquire_file('http://search.twitter.com/search.atom?geocode=32.71533,-117.157256,10mi&lang=en&include_entities=true&result_type=recent')); */
 ?>
+
