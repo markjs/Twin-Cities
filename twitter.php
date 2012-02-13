9a603a71573d7e011bcc44ce3755914074152d26
Charlie Tizard <c2-tizard@cems.uwe.ac.uk>
2012-02-13 10:24:20 +0000

    LITERALLY changed my twitter code

--- twitter.php
+++ twitter.php
@@ -5,13 +5,13 @@
 include("commonlib.php");
 loadConfig("config.xml");
 
-$ed = $city1lat . "," . $city1lng;
+$ed = $city1Lat . "," . $city1Lng;
 
-$sd = $city2lat . "," . $city2lng;
+$sd = $city2Lat . "," . $city2Lng;
 
 $twitter2 = simplexml_load_string(acquire_file('http://search.twitter.com/search.atom?geocode=' . $sd . ',10mi&lang=en&include_entities=true&result_type=recent'));
 
-$twitter = simplexml_load_string(acquire_file('http://search.twitter.com/search.atom?geocode=32.71533,-117.157256,10mi&lang=en&include_entities=true&result_type=recent'));
+/* $twitter = simplexml_load_string(acquire_file('http://search.twitter.com/search.atom?geocode=32.71533,-117.157256,10mi&lang=en&include_entities=true&result_type=recent')); */
 
 print_r ($twitter);
 
