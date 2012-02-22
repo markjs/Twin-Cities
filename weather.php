85007e3d0c52c0c3dba4579ce6ac915ac53db251
Mark Smith <mark@markjs.net>
2012-02-22 16:07:08 +0000

    weather works like a boss

--- weather.php
+++ weather.php
@@ -12,15 +12,44 @@ function weatherGetCurrentWeather($cityName) {
 	
 	$response = simplexml_load_string(acquire_file($request));
 	
-	$summary = $response->weather->current_conditions->condition['data'];
-	$temp = $response->weather->current_conditions->temp_c['data'];
-	$imgUrl = "http://google.com/" . $response->weather->current_conditions->icon['data'];
+	$conditions = $response->weather->current_conditions;
+	
+	$summary = $conditions->condition['data'];
+	$temp = $conditions->temp_c['data'];
+	$imgUrl = "http://google.com/" . $conditions->icon['data'];
 	
 	$html = "<img src=\"$imgUrl\" alt=\"$summary\"/><p>$summary</p><p>$temp</p>";
 		
 	return $html;
 }
 
-echo weatherGetCurrentWeather("san diego");
+
+function weatherGetFutureWeather($cityName,$daysFromNow) {
+	
+	$config = loadConfig();
+	$weather = $config['weather'];
+				
+	$request = $weather['baseUri'] . urlencode($cityName);
+	
+	$response = simplexml_load_string(acquire_file($request));
+	
+	$conditions = $response->weather->forecast_conditions[$daysFromNow];
+	
+	$summary = $conditions->condition['data'];
+	$temp_low = farenheitToCelcius($conditions->low['data']);
+	$temp_high = farenheitToCelcius($conditions->high['data']);
+	$imgUrl = "http://google.com/" . $conditions->icon['data'];
+	
+	$html = "<img src=\"$imgUrl\" alt=\"$summary\"/><p>$summary</p><p>$temp_low</p><p>$temp_high</p>";
+		
+	return $html;
+}
+
+function farenheitToCelcius($temp) {
+	return round(($temp-32)*(5/9));
+}
+
+
+echo weatherGetFutureWeather("san diego",0);
 
 ?>
