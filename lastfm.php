81fe25bb66dd671c81007a67cd35be515e001484
Alexander Jegtnes <as2-jegtnes@cems.uwe.ac.uk>
2012-02-22 11:07:26 +0000

    Added final comments to last.fm and news

--- lastfm.php
+++ lastfm.php
@@ -3,32 +3,37 @@
 include_once("commonlib.php");
 
 /**
- *
+ * @
  * @param type $num The number of top artists to return
  * @param type $lfmMetro The city to get the top artists from
  * @param type $lfmCountry The country the city is in
- * @author Alexander Jegtnes
- * @version 1.0
+ * @author Alexander Jegtnes <alexander2.jegtnes@live.uwe.ac.uk>
+ * @version 2.0
  * @return array Returns an associative array with the top artists, the artist picture and the artist URL
  */
 
 function lastfmGetTopArtists($num, $lfmMetro, $lfmCountry) {
 	
+	//loads configuration and sets up initial variables
 	$config = loadConfig();
-	
 	$count = 0;
 	$ret =""; 
-	
 	$lastfmApiKey = $config['lastfm']['apiKey'];
 	$lastfmMethod = $config['lastfm']['method'];
 	
 	//assembles the request URI for the API
 	@$lastfmUri = "http://ws.audioscrobbler.com/2.0/?method=" . $lastfmMethod . "&metro=" . urlencode($lfmMetro) . "&country=" . urlencode($lfmCountry) . "&api_key=" . $lastfmApiKey;
 	
+	//gets the XML data
 	$lastfmData = simplexml_load_string(acquire_file($lastfmUri));
+	
+	//loops through it and makes sure to only get as many artists as is specified
+	//the count is needed because the last.fm API doesn't easily provide the ability to return a set number of artists
+	
 	foreach($lastfmData->topartists->artist as $artistName) {
 		$count++;
 		if ($count <= $num) {
+			
 			//the casting is needed because SimpleXML objects are complex objects.
 			//http://www.php.net/manual/en/ref.simplexml.php#91057
 			$ret[] = array((string) 'name' => $artistName->name[0], 'image' => $artistName->image[2], 'url' => $artistName->url[0]);
@@ -36,10 +41,5 @@ function lastfmGetTopArtists($num, $lfmMetro, $lfmCountry) {
 	}
 	return($ret);
 }
-
-//echo "<pre>";
-//print_r(lastfmGetTopArtists(10, "Edinburgh", "United Kingdom"));
-//echo "</pre>";
-
 
 ?>
--- news.php
+++ news.php
@@ -11,16 +11,22 @@ include_once("commonlib.php");
  */
 
 function acquireNews($searchTerm) {
+	
+	//initialises variables
 	$config = loadConfig();
     $returnContent="";
 	
 	//assembles the request URI to the API
     $newsUri = $config['news']['baseUri'] . "?format=xml" . "&q=" . urlencode($searchTerm) . "&order-by=" . urlencode($config['news']['orderBy']) . "&page-size=" . $config['news']['numberOfResults'] . "&date-id=" . urlencode("date/" . $config['news']['dates']);
 	
+	//loads all the XML data
     $newsData = simplexml_load_string(acquire_file($newsUri));
 	
+	//loops through the data as required
 	foreach($newsData->results as $newsNode) {
 		foreach ($newsNode as $newsDetail) {
+			//the string casting is needed because SimpleXML objects are complex objects.
+			//http://www.php.net/manual/en/ref.simplexml.php#91057
 			$returnContent[] = array((string) 'title' => $newsDetail['web-title'], 'url' => ($newsDetail['web-url']), 'date' => $newsDetail['web-publication-date']);
 		}
 	}
