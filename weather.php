7ddf16de54e0c10e1837aeb1be230f842adbe956
unknown <Charlie@Charlie-PC.(none)>
2012-02-22 19:19:10 +0000

    we're finished. <br> gone, padding added. FUCK YES.

--- weather.php
+++ weather.php
@@ -41,7 +41,7 @@ function weatherGetFutureWeather($cityName,$daysFromNow) {
 	$temp_high = farenheitToCelcius($conditions->high['data']);
 	$imgUrl = "http://google.com/" . $conditions->icon['data'];
 	
-	$html = "<h5 class=\"day\">$day</h5><img src=\"$imgUrl\" alt=\"$summary\" class=\"weatherpicturesmall\"/><p class=\"summarysmall\">Outlook: $summary, Low: $temp_low&#8451;, High: $temp_high&#8451;</p> <pclass=\"summarysmall\"></p></br>";
+	$html = "<h5 class=\"day\">$day</h5><img src=\"$imgUrl\" alt=\"$summary\" class=\"weatherpicturesmall\"/><p class=\"summarysmall\">Outlook: $summary, Low: $temp_low&#8451;, High: $temp_high&#8451;</p>";
 		
 	return $html;
 }
