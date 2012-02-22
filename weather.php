5fe5f170f9162e62977479723c1780f95557be07
Mark Smith <mark@markjs.net>
2012-02-22 15:45:26 +0000

    YEAH THAT'S WEATHER WORKING WOOP

--- weather.php
+++ weather.php
@@ -2,8 +2,19 @@
 
 include_once("commonlib.php");
 
-function weatherGetCurrentWeather($lat,$lng) {
+
+function weatherGetCurrentWeather($cityName) {
 	
+	$config = loadConfig();
+	$weather = $config['weather'];
+				
+	$request = $weather['baseUri'] . urlencode($cityName);
+	
+	$response = simplexml_load_string(acquire_file($request));
+		
+	return $response->weather->current_conditions->condition['data'];
 }
+
+echo weatherGetCurrentWeather("edinburgh");
 
 ?>
