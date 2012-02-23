866f4efb146f9e16e32a86759a9c4f4c4da66d41
Mark Smith <mark@markjs.net>
2012-02-23 12:27:05 +0000

    commented instagram and weather

--- instagram.php
+++ instagram.php
@@ -1,37 +1,34 @@
 <?php
 
-include_once("commonlib.php");
-
-
-// https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=ACCESS-TOKEN
-
 function instagramGetLatestPhotos($lat,$lng) {
 	
+	// Loading configxml into variable
 	$config = loadConfig();
-	
+	// Storing instagram specific configs for more readable access
 	$instagram = $config['instagram'];
 	
+	// Forming the request URI
 	$request = $instagram['baseUri'] . "lat=" . $lat . "&lng=" . $lng . "&client_id=" . $instagram['clientId'];
 	
+	// Requesting the URI and decoding the response into a PHP object
 	$response = json_decode(acquire_file($request));
 	
-	// echo "<pre>";
-	// 	print_r($response);
-	
+	// Initialises $html variable so it can be appended to with the loop
 	$html = "";
-
+	// Initialises counter for the loop so the count can be limited
 	$i = 0;
 
+	// Foreach used so that if less than 4 items are returned the loop will stop
 	foreach ($response->data as $entry) {
 		if ($i < 4) {
+			// Image data appended to $html, ready to be printed
 			$html .= "<a href=\"" . $entry->link . "\"><img src=\"" . $entry->images->thumbnail->url . "\" alt=\"" . $entry->caption->text . "\"/></a>";
 		}
 		$i++;
 	}
-	
+
+	// $html returned, to be printed when the function is called
 	return $html;
 }
-
-// instagramGetLatestPhotos(55.953237,-3.188438);
 
 ?>
--- weather.php
+++ weather.php
@@ -1,52 +1,68 @@
 <?php
 
-include_once("commonlib.php");
-
-
 function weatherGetCurrentWeather($cityName) {
 	
+	// Loading configxml into variable
 	$config = loadConfig();
+	// Storing weather specific configs for more readable access
 	$weather = $config['weather'];
-				
+	
+	// Forming the request URI
 	$request = $weather['baseUri'] . urlencode($cityName);
 	
+	// Requesting the URI and decoding the response into a PHP object
 	$response = simplexml_load_string(acquire_file($request));
 	
+	// Storing current conditions into a variable for more readable code
 	$conditions = $response->weather->current_conditions;
 	
+	// Getting specific data and storing into variables ready for printing
 	$summary = $conditions->condition['data'];
 	$temp = $conditions->temp_c['data'];
 	$imgUrl = "http://google.com/" . $conditions->icon['data'];
 	
+	// Forming the $html variable to be printed
 	$html = "<img src=\"$imgUrl\" alt=\"$summary\" class=\"weatherpicture\"/><h5 class=\"summary\">$summary, $temp&#176;C</h5><div class=\"clear\"></div>";
-		
+
+	// $html returned, to be printed when the function is called		
 	return $html;
 }
 
 
 function weatherGetFutureWeather($cityName,$daysFromNow) {
 	
+	// Loading configxml into variable
 	$config = loadConfig();
+	// Storing weather specific configs for more readable access
 	$weather = $config['weather'];
-				
-	$request = $weather['baseUri'] . urlencode($cityName);
 	
+	// Forming the request URI					
+	$request = $weather['baseUri'] . urlencode($cityName);
+
+	// Requesting the URI and decoding the response into a PHP object	
 	$response = simplexml_load_string(acquire_file($request));
 	
+	// Storing the forecast conditions into a variable for more readable code
+	// Including $daysFromNow variable to return forecast for a specific day
 	$conditions = $response->weather->forecast_conditions[$daysFromNow];
-	
+
+	// Getting specific data and storing into variables ready for printing	
 	$summary = $conditions->condition['data'];
 	$day = $conditions->day_of_week['data'];
+	// Passing temperatures through the unit conversion function
 	$temp_low = fahrenheitToCelsius($conditions->low['data']);
 	$temp_high = fahrenheitToCelsius($conditions->high['data']);
 	$imgUrl = "http://google.com/" . $conditions->icon['data'];
 	
+	// Forming the $html variable to be printed
 	$html = "<h5 class=\"day\">$day</h5><img src=\"$imgUrl\" alt=\"$summary\" class=\"weatherpicturesmall\"/><p class=\"summarysmall\">Outlook: $summary, Low: $temp_low&#176;C, High: $temp_high&#176;C</p>";
-		
+	
+	// $html returned, to be printed when the function is called	
 	return $html;
 }
 
 function fahrenheitToCelsius($temp) {
+	// Simply reads in a number and returns the converted and rounded value
 	return round(($temp-32)*(5/9));
 }
 
